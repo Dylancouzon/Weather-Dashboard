980017f0df3152620f108f862657414ae8fa2815
Dylancouzon <dylancouzon@gmail.com>
2021-04-02 00:46:06 -0700

    complete searchCity function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-// key 7e3a149deb7dcf451641dcd1d05f5cd5
+var searchBtn = $('button');
+var inputEl = $('.form-control');
 var cityName = "London";
+var searchHistory = JSON.parse(localStorage.getItem("search"))
+if(!searchHistory){
+  var searchHistory = [];
+} 
+// Search Function
+function searchCity() {
+  var cityName = inputEl.val();
+  getWeather(cityName);
+  searchHistory.push(cityName);
+  localStorage.setItem("search",JSON.stringify(searchHistory));
+}
 
-function getWeather() {
+// Fetch the API
+function getWeather(cityName) {
+  // key 7e3a149deb7dcf451641dcd1d05f5cd5
   var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7e3a149deb7dcf451641dcd1d05f5cd5";
   fetch(url)
     .then(function (response) {
@@ -12,4 +26,4 @@ function getWeather() {
       });
 }
 
-getWeather();
+searchBtn.on('click', searchCity);
