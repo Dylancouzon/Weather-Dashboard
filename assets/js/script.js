bc616f604b0a5d7f50887b61db0b53dbd5ee125c
Dylancouzon <dylancouzon@gmail.com>
2021-04-01 22:41:56 -0700

    add basic Jumbotron elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-// key f6da59a3955cc8be60230362816f2173
+// key 7e3a149deb7dcf451641dcd1d05f5cd5
 
 function getWeather() {
-  var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=f6da59a3955cc8be60230362816f2173";
+  var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=7e3a149deb7dcf451641dcd1d05f5cd5";
   fetch(url)
     .then(function (response) {
       return response.json();
     })
       .then(function (data) {
-      
-      }
+     // console.log(data);
+      });
 }
+
+getWeather();
