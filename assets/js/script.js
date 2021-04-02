404792894abf20e8e239ef85151405bb4e89c2e0
Dylancouzon <dylancouzon@gmail.com>
2021-04-01 23:47:40 -0700

    add city list + Search Bootstrap menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 // key 7e3a149deb7dcf451641dcd1d05f5cd5
+var cityName = "London";
 
 function getWeather() {
-  var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=7e3a149deb7dcf451641dcd1d05f5cd5";
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7e3a149deb7dcf451641dcd1d05f5cd5";
   fetch(url)
     .then(function (response) {
       return response.json();
     })
       .then(function (data) {
-     // console.log(data);
+     console.log(data);
       });
 }
 
